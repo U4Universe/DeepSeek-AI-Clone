552da8bc0ce9e37d4b574623b891c01eeab51c69
SHWETA JHA <150259603+U4Universe@users.noreply.github.com>
2025-04-18 21:54:45 +0530

    ChatLabel.jsx

--- components/ChatLabel.jsx
+++ components/ChatLabel.jsx
@@ -1,51 +1,101 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { assets } from '@/assets/assets'
-import Image from 'next/image'
+import { assets } from "@/assets/assets";
+import { useAppContext } from "@/context/AppContext";
+import axios from "axios";
+import Image from "next/image";
+import React from "react";
+import toast from "react-hot-toast";
 
-const ChatLabel = () => {
-    const [menuOpen, setMenuOpen] = useState(false)
-    const menuRef = useRef(null)
+const ChatLabel = ({ openMenu, setOpenMenu, id, name }) => {
+  const { fetchUsersChats, chats, setSelectedChat } = useAppContext();
 
-    // Close dropdown if clicking outside
-    useEffect(() => {
-        const handleClickOutside = (event) => {
-            if (menuRef.current && !menuRef.current.contains(event.target)) {
-                setMenuOpen(false)
-            }
-        }
+  const selectChat = () => {
+    const chatData = chats.find((chat) => chat._id === id);
+    setSelectedChat(chatData);
+    console.log(chatData);
+  };
 
-        document.addEventListener('mousedown', handleClickOutside)
-        return () => document.removeEventListener('mousedown', handleClickOutside)
-    }, [])
+  const renameHandler = async () => {
+    try {
+      const newName = prompt("Enter new name");
+      if (!newName) return;
+      const { data } = await axios.post("/api/chat/rename", {
+        chatId: id,
+        name: newName,
+      });
+      if (data.success) {
+        fetchUsersChats();
+        setOpenMenu({ id: 0, open: false });
+        toast.success(data.message);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
-    return (
-        <div className="group flex items-center justify-between p-2 text-white/80 hover:bg-white/10 rounded-lg text-sm cursor-pointer">
-            <p className="truncate max-w-[85%]">Chat Name Here</p>
+  const deleteHandler = async () => {
+    try {
+      const confirm = window.confirm(
+        "Are you sure you want to delete this chat?"
+      );
+      if (!confirm) return;
+      const { data } = await axios.post("/api/chat/delete", { chatId: id });
+      if (data.success) {
+        fetchUsersChats();
+        setOpenMenu({ id: 0, open: false });
+        toast.success(data.message);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
-            {/* Three dots and menu */}
-            <div className="relative" ref={menuRef}>
-                <div
-                    onClick={() => setMenuOpen(!menuOpen)}
-                    className="hidden group-hover:flex items-center justify-center h-6 w-6 aspect-square hover:bg-black/80 rounded-lg cursor-pointer"
-                >
-                    <Image src={assets.three_dots} alt="" className="w-4" />
-                </div>
-
-                {menuOpen && (
-                    <div className="absolute -right-36 top-6 bg-gray-700 rounded-xl w-max p-2 z-10">
-                        <div className="flex items-center gap-3 hover:bg-white/10 px-3 py-2 rounded-lg cursor-pointer">
-                            <Image src={assets.pencil_icon} alt="" className="w-4" />
-                            <p>Rename</p>
-                        </div>
-                        <div className="flex items-center gap-3 hover:bg-white/10 px-3 py-2 rounded-lg cursor-pointer">
-                            <Image src={assets.delete_icon} alt="" className="w-4" />
-                            <p>Delete</p>
-                        </div>
-                    </div>
-                )}
-            </div>
+  return (
+    <div
+      onClick={selectChat}
+      className="flex items-center justify-between p-2 text-white/80 hover:bg-white/10 rounded-lg text-sm group cursor-pointer"
+    >
+      <p className="group-hover:max-w-5/6 truncate">{name}</p>
+      <div
+        onClick={(e) => {
+          e.stopPropagation();
+          setOpenMenu({ id: id, open: !openMenu.open });
+        }}
+        className="group relative flex items-center justify-center h-6 w-6 aspect-square hover:bg-black/80 rounded-lg"
+      >
+        <Image
+          src={assets.three_dots}
+          alt="three dots"
+          className={`w-4 ${
+            openMenu.id === id && openMenu.open ? "" : "hidden"
+          } group-hover:block`}
+        />
+        <div
+          className={`absolute ${
+            openMenu.id === id && openMenu.open ? "block" : "hidden"
+          } -right-36 top-6 bg-gray-700 rounded-xl w-max p-2`}
+        >
+          <div
+            onClick={renameHandler}
+            className="flex items-center gap-3 hover:bg-white/10 px-3 py-2 rounded-lg"
+          >
+            <Image src={assets.pencil_icon} alt="pencil icon" className="w-4" />
+            <p>Rename</p>
+          </div>
+          <div
+            onClick={deleteHandler}
+            className="flex items-center gap-3 hover:bg-white/10 px-3 py-2 rounded-lg"
+          >
+            <Image src={assets.delete_icon} alt="delete icon" className="w-4" />
+            <p>Delete</p>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ChatLabel
+export default ChatLabel;
