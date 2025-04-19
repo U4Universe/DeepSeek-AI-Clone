190760b8208fb09b324bffc6f600670cbc678ef5
SHWETA JHA <150259603+U4Universe@users.noreply.github.com>
2025-04-19 11:56:07 +0530

    Chat.js

--- model/Chat.js
+++ model/Chat.js
@@ -1,15 +1,10 @@
 import mongoose from "mongoose";
 
-const ChatSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    messages: [
-        {
-            role: { type: String, required: true },
-            content: { type: String, required: true },
-            timestamp: { type: Number, required: true },
-        },
-    ],
+const ChatSchema = new mongoose.Schema({
+  userId: String,
+  messages: [{ role: String, content: String }],
+  createdAt: { type: Date, default: Date.now },
+});
 
     userId: { type: String, required: true },
     
